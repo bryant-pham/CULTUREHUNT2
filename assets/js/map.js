364c339ad18e08930a75ffb1733bed393e7c8592
Priscilla Lam <priscillaklam@gmail.com>
2014-06-01 02:55:46 -0500

    CheckIn Function

--- assets/js/map.js
+++ assets/js/map.js
@@ -39,6 +39,45 @@
         handleNoGeolocation(false);
       }
     }
+    
+    function checkIn(checkinLat, checkinLong)  {
+      var currentLat;
+      var currentLong;
+      //get the current position again to see if it has changed.
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          currentLat = pos.lat(); //lat1
+          currentLong = pos.lng(); //long1
+
+          //HAVERSINE FORMULA TO CALCULATE DISTANCE
+          var R = 6371; // Radius of the earth in km
+          var dLat = deg2rad(checkinLat-currentLat);  // deg2rad below
+          var dLon = deg2rad(checkinLong-currentLong); 
+          var a = 
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(currentLat)) * Math.cos(deg2rad(checkinLat)) * 
+            Math.sin(dLon/2) * Math.sin(dLon/2)
+          ; 
+          var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+          var d = R * c * 1000; // Distance in meters
+
+          //Check In Validation Condition
+          if (d<=400) {
+            alert("YOU CHECKED IN MANG");
+          } else {
+            alert("You are not close enough to check in.");
+          }
+          
+        }, function() {
+          handleNoGeolocation(true);
+        });
+      } 
+      else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+      }
+    }
 
     //creates marker using name, lats, and longs
     function setLocalMarkers(markerName, markerLat, markerLong) {
